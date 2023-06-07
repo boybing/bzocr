6256e3d6f68ef748d26ef06a21fca4919452e57b
boybing <ybxxx070@126.com>
2023-06-07 16:59:01 +0800

    Update reading.py

--- static/file/reading.py
+++ static/file/reading.py
@@ -1,6 +1,7 @@
 # 导入必要的库
 from gtts import gTTS  # 用于在线语音合成
 import os  # 用于操作系统命令
+import sys  # 用于获取命令行参数
 
 # 设置语言和速度
 language = "zh"  # 中文
@@ -21,14 +22,15 @@ def read_file(filename):
 # 定义一个主函数，用于获取用户输入的文本或文件名并调用speak函数
 def creatMp3(input_text):
 
-    if input_text == "q":  # 如果用户输入q，退出程序
-        pass
-    elif input_text.endswith(".txt"):  # 如果用户输入以.txt结尾，认为是一个文本文件名，调用read_file函数读取文件内容，并赋值给text变量
+    if input_text.endswith(".txt"):  # 如果用户输入以.txt结尾，认为是一个文本文件名，调用read_file函数读取文件内容，并赋值给text变量
         text = read_file(input_text)
     else:  # 否则，认为是一个文本，直接赋值给text变量
         text = input_text
-        speak_online(text)  # 调用speak_online函数将文本转换为语音并播放（在线）
+
+    speak_online(text)  # 调用speak_online函数将文本转换为语音并播放（在线）
 
 # 调用主函数
 if __name__ == "__main__":
-    creatMp3('111')
+    if len(sys.argv) > 1:  # 如果有命令行参数
+        input_text = sys.argv[1]  # 获取第一个命令行参数作为输入文本
+        creatMp3(input_text)  # 调用creatMp3函数并传递输入文本
