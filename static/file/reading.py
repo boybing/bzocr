4bce3df74637f8143455c2b9b41bac93c36f6ee2
boybing <ybxxx070@126.com>
2023-06-08 18:19:11 +0800

    Update reading.py

--- static/file/reading.py
+++ static/file/reading.py
@@ -3,6 +3,7 @@ from gtts import gTTS  # 用于在线语音合成
 from pydub import AudioSegment
 import os  # 用于操作系统命令
 import sys  # 用于获取命令行参数
+import rdm
 
 # 设置语言和速度
 language = "zh"  # 中文
@@ -12,20 +13,34 @@ speed = 150  # 语速
 def speak_online(tt):
     # 将文本拆分为多个部分
     # 创建一个空的音频片段
-    combined = AudioSegment.empty()
-    # 遍历每个部分
-    tts = gTTS(text=tt, lang="zh")
-    tts.save("tst.mp3")
-    speech = AudioSegment.from_mp3("tst.mp3")
-    # 将语音添加到组合音频中
-    combined += speech
-    combined += AudioSegment.silent(duration=3000)
-    tts = gTTS(text='测试测试', lang="zh")
-    tts.save("tst1.mp3")
-    speech = AudioSegment.from_mp3("tst1.mp3")
-    combined += speech
-    # 保存组合音频
-    combined.export("temp.mp3", format="mp3")
+    if tt='rd':
+        answer=rdm.rd()
+        combined = AudioSegment.empty()
+        for an in answer:
+            tts = gTTS(text=an, lang="zh")
+            tts.save("temp.mp3")
+            speech = AudioSegment.from_mp3("tst.mp3")
+            # 将语音添加到组合音频中
+            combined += speech
+            combined += AudioSegment.silent(duration=5000)
+            combined.export("temp.mp3", format="mp3")
+    else:
+        combined = AudioSegment.empty()
+        # 遍历每个部分
+        tts = gTTS(text=tt, lang="zh")
+        tts.save("tst.mp3")
+        speech = AudioSegment.from_mp3("tst.mp3")
+        # 将语音添加到组合音频中
+        combined += speech
+        combined += AudioSegment.silent(duration=3000)
+        tts = gTTS(text='测试测试', lang="zh")
+        tts.save("tst1.mp3")
+        speech = AudioSegment.from_mp3("tst1.mp3")
+        combined += speech
+        # 保存组合音频
+        combined.export("temp.mp3", format="mp3")
+
+    
 
 # 定义一个函数，用于读取文本文件并返回文件内容
 def read_file(filename):
