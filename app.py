b1315a9a6685b06f95b1f91aef9326940e708117
boybing <ybxxx070@126.com>
2023-09-15 19:49:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -98,6 +98,24 @@ def word():
     pdf_path = url_for('static', filename='file/m.pdf')
     return render_template('pdf.html', pdf_path=pdf_path)
 
+@app.route('/word1', methods=['POST'])
+def word():
+    # 获取表单提交的数据
+    input = request.form.get('input')
+    parent=os.getcwd()
+    os.chdir("static")
+    os.chdir("file")
+    try:
+        os.remove("m.pdf")
+        print(os.getcwd())
+    except Exception as e:
+        print(e)
+    
+    os.system("python xieci.py nopinyin "+input)
+    os.chdir(parent)
+    pdf_path = url_for('static', filename='file/m.pdf')
+    return render_template('pdf.html', pdf_path=pdf_path)
+
 @app.route('/mp3', methods=['POST'])
 def mp3():
     # 获取表单提交的数据
